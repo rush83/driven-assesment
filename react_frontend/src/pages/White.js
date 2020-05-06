64e01276736ad92e9ed062e227bf8e4dea944a73
rush83 <mohamadrasheedy@gmail.com>
2020-05-06 16:27:44 -0700

    2nd task done

--- react_frontend/src/pages/White.js
+++ react_frontend/src/pages/White.js
@@ -6,7 +6,9 @@ export default class White extends Component {
         super(props)
         this.state = {
             num: 0,
-            arrayList: []
+            arrayList: [],
+            text: '',
+            color: ''
         }
     }
 
@@ -27,7 +29,17 @@ export default class White extends Component {
 
     }
 
+    changeText = (e) => {
+        this.setState({
+            text: e.target.value
+        })
+    }
 
+    changeColor(e) {
+        this.setState({
+            color: e.target.value
+        })
+    }
 
 
     render() {
@@ -44,7 +56,17 @@ export default class White extends Component {
                         {this.state.arrayList.map(item =>
                             <tr key={item}>
                                 {
-                                    this.state.arrayList.map(col => col % 2 == 0 ? <td style={{ padding: '10px', backgroundColor: 'white', height: '80px' }}></td> : <td style={{ padding: '10px', backgroundColor: 'black', height: '80px' }}></td>
+                                    this.state.arrayList.map(col =>
+                                        col % 2 === 0 ? <td align="center" style={{ padding: '10px', backgroundColor: 'white', height: '80px' }}>
+                                            {item === 0 || item === 1 ?
+                                                <h1 style={{ color: this.state.color === '' ? '#f00' : this.state.color }}>{this.state.text === '' ? 'red' : this.state.text}</h1>
+                                                : (item === this.state.arrayList.length - 1 || item === this.state.arrayList.length - 2) ? <h1 style={{ color: this.state.color === '' ? '#000' : this.state.color }}>{this.state.text === '' ? 'black' : this.state.text}</h1> : ''}
+                                        </td> :
+                                            <td align="center" style={{ padding: '10px', backgroundColor: 'black', height: '80px' }}>
+                                                {item === 0 || item === 1 ?
+                                                    <h1 style={{ color: this.state.color === '' ? '#f00' : this.state.color }}>{this.state.text === '' ? 'red' : this.state.text}</h1>
+                                                    : (item === this.state.arrayList.length - 1 || item === this.state.arrayList.length - 2) ? <h1 style={{ color: this.state.color === '' ? '#000' : this.state.color }}>{this.state.text === '' ? 'black' : this.state.text}</h1> : ''}
+                                            </td>
 
                                     )
                                 }
@@ -52,6 +74,10 @@ export default class White extends Component {
                         )}
                     </tbody>
                 </table>
+
+                Text <input name="text_change" type="radio" value="Done" onChange={(event) => this.changeText(event)} /> Reset <input name="text_change" type="radio" value="" onChange={(event) => this.changeText(event)} />
+                <br />
+                Color <input name="color_change" type="radio" value="green" onChange={(event) => this.changeColor(event)} /> Reset <input name="color_change" type="radio" value="" onChange={(event) => this.changeColor(event)} />
 
             </div>
         )
